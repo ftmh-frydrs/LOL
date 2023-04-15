723f757b94f8bf5f354f42d540fc0e086aa7e0cd
Fatemeh <fatemehfaryadras7@gmail.com>
2023-04-15 12:58:47 +0330

    first commit

---
+++ content/js/script.js
@@ -0,0 +1,51 @@
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+     var swiper3 = new Swiper(".mySwiper3", {
+            slidesPerView: 2,
+            spaceBetween: 20,
+            rewind: true,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            breakpoints: {
+                640: {
+                    slidesPerView: 3,
+                    spaceBetween: 20,
+                },
+                768: {
+                    slidesPerView: 3,
+                    spaceBetween: 40,
+                },
+                1024: {
+                    slidesPerView: 4,
+                    spaceBetween: 50,
+                },
+            },
+        });
+
+
+
+        
+var sideMenu = document.getElementById('side-menu');
+        function openMenu() {
+            sideMenu.classList.remove('left-[-250px]');
+            sideMenu.classList.add('left-0');
+        }
+
+        function closeMenu() {
+            sideMenu.classList.remove('left-0');
+            sideMenu.classList.add('left-[-100%]');
+        }        
